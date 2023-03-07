60882eda1d014eafe7d8fbd73335a778e8b855a4
networkdavit <lonecreation1612@gmail.com>
2023-03-07 21:54:03 +0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Davit Hayrapetyan on 07.03.23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
